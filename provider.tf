9b9eea426054e5a3fc3bd50c8d4960adcff20c1a
David McFarland <damcfarl@cisco.com>
2024-11-09 11:50:10 -0800

    Initial tf files

---
+++ provider.tf
@@ -0,0 +1,19 @@
+/*
+Sample file.tf to connect to Intersight with API Key 
+and request the Intersight Terraform Provider
+*/
+ 
+terraform {
+  required_providers {
+    intersight = {
+      source = "CiscoDevNet/intersight" /* Cisco intersight Provider */
+      version = ">=1.0.35" /* request version equal to or greater */
+    }
+  }
+}
+
+provider "intersight" {
+  apikey    = "59ce895f16267c000103c296/60706e367564612d30808a36/672bb1df756461300102e52f"
+  secretkey = "../secrets/intersight_secret_key"
+  endpoint = "https://intersight.com"
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "org1_name" {
+  type    = string
+  default = "Compute"
+}
+
+variable "server_profile" {
+  type    = string
+  default = "damcfarl-server-profile"
+}
+
+variable "ntp1_name" {
+  type    = string
+  default = "damcfarl-ntp"
+}
+
+variable "account" {
+  type    = string
+  default = "Dave Mcfarland"
+# default = "damcfarl@cisco.com"
+}
+
+variable "resource_group" {
+  type    = string
+  default = "Compute-RG"
+}
+
