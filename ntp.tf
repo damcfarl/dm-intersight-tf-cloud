2d2471b9ad68b3ba523841e4e3dcdb475c57d202
David McFarland <damcfarl@cisco.com>
2024-12-05 09:54:32 -0800

    add ntp policy

---
+++ ntp.tf
@@ -0,0 +1,20 @@
+/* create an ntp policy and add to an org */
+
+data "intersight_organization_organization" "myorg1" {
+  name = var.org1_name
+}
+
+resource "intersight_ntp_policy" "myntp" {
+  name        = var.ntp1_name
+  description = var.ntp1_name
+  enabled     = true
+  ntp_servers = [
+    "ntp.esl.cisco.com",
+    "time-a-g.nist.gov",
+    "time-b-g.nist.gov"
+  ]
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.myorg1.results[0].moid
+  }
+}
